f8b6739e78e472580f9e3532f3b8a40eea596dd2
guillaume_collet <lomig76@yahoo.fr>
2019-10-02 09:07:49 +0200

    1er commit

---
+++ src/main/java/M2_CCN_TAA_GLI_PROJECT/M2_CCN_TAA_GLI_PROJECT_WEEK_END/App.java
@@ -0,0 +1,13 @@
+package M2_CCN_TAA_GLI_PROJECT.M2_CCN_TAA_GLI_PROJECT_WEEK_END;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
